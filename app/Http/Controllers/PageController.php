10f000e5d56fe7c68a3a4522e07ffeae5914f199
BeneArp <benedetta.arpino@gmail.com>
2024-09-12 17:41:29 +0200

    feat: add train model to get data from database

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Train;
 
 class PageController extends Controller
 {
     public function home(){
+
+        $trains = Train::all();
+        // dump($trains);
+
         return view('home');
     }
 
