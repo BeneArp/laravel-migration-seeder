c18c4f07431e59986de71a447f4cdff68e18efbc
BeneArp <benedetta.arpino@gmail.com>
2024-09-12 16:39:56 +0200

    build: add base project files

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,13 @@
 {{-- questa view estende il file main.blade.php che è dentro la cartella view/layouts --}}
 @extends('layouts.main')
 
-@section('hero')
-    <div>HERO HOME</div>
-@endsection
 
 @section('content')
 <div class="container my-5">
-    <h1>{{ $title }}</h1>
-    <img src="{{ Vite::asset('resources/img/colibri.jpg') }}" alt="" class="img-fluid">
+    <h1>Home</h1>
+
     <p>
-        {{ $text }}
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quaerat atque reprehenderit autem dolorem repellendus numquam iste eum tenetur omnis nisi aut, veritatis molestiae adipisci ab ipsum voluptatem eaque voluptatum.
     </p>
 </div>
 
